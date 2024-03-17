437d900de090680a6880be32963b5a7bd0433d9a
refilwe-m <refilwe.dev@gmail.com>
2024-03-17 19:57:58 +0200

    Exposed server with ngrok to public use

--- ast-server/src/app.js
+++ ast-server/src/app.js
@@ -18,35 +18,29 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "src/public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production", // Enable SSL in production
+  ssl: process.env.NODE_ENV === "production",
 });
-
-// Your Express routes and middleware go here
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
